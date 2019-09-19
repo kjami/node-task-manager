f47b77f35e9819b8cd3a385e0332ebbaf4ed49b1
kjami <kishorjami@gmail.com>
2019-09-19 19:18:30 +0800

    CORS setup

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ const app = express();
 // app.use((req, res, next) => {
 //     res.status(503).send("Currently under maintenance.");
 // });
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    next();
+};
+
+app.use(allowCrossDomain);
 
 app.use(express.json());
 
