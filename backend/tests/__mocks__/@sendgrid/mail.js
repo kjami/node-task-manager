3515620416cd12a315db114e9950aae4bfb3c49f
kjami <kishorjami@gmail.com>
2019-09-22 17:54:50 +0800

    Add mocking to send emails for testing.

---
+++ backend/tests/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,12 @@
+/* global module */
+module.exports = { 
+    setApiKey() {
+        return true;
+    }, 
+    send() {
+        // eslint-disable-next-line no-unused-vars
+        return new Promise((resolve, reject) => {
+            resolve(true);
+        })
+    } 
+};
